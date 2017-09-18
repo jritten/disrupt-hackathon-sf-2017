82eb57416122713e17df98b7abd7f2488b8c3cc1
Kanij Yomoda <kanji@mbp.local>
2017-09-18 16:25:07 -0700

    Fix parent's show page

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
     var hours = now.getHours();
     var minutes = now.getMinutes();
     var val = $('textarea').val();
-      $('.messages').append('<section class="card message"><div class="card-block"><span class="card-title">Janna</span><span class="sent-time">' + hours + ':' + minutes + 'PM</span><p class="card-text">' + val + '</p></div></section>');
+    var name = $('h2').text();
+      $('.messages').append('<section class="card message"><div class="card-block"><span class="card-title">Rebecca</span><span class="sent-time">' + hours + ':' + minutes + 'PM</span><p class="card-text">' + val + '</p></div></section>');
       $(".messages").scrollTop($(".messages")[0].scrollHeight);
       $('textarea').val(" ");
   });
